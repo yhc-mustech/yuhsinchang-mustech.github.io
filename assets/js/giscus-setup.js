16f0bb31294e45526e89d151a647894709a49cc7
yhc-mustech <162872429+yhc-mustech@users.noreply.github.com>
2026-04-25 08:34:23 +0000

    Deploying to gh-pages from @ yhc-mustech/yuhsinchang-mustech.github.io@84a34075bce9d4f54a82f043522d247f28ca8c90 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "yuhsinchang-mustech/yuhsinchang-mustech.github.io",
+    "data-repo": "yhc-mustech/yuhsinchang-mustech.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
